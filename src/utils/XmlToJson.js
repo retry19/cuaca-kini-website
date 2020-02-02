98c360fd8fd9715b1bcc5996614901692e8f049f
retry19 <rezarahmanudin@gmail.com>
2020-02-02 10:40:28 +0700

    xml to json

---
+++ src/utils/XmlToJson.js
@@ -0,0 +1,39 @@
+function xmlToJson(xml) {
+	
+	// Create the return object
+	let obj = {};
+
+	if (xml.nodeType === 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		obj["attr"] = {};
+			for (let j = 0; j < xml.attributes.length; j++) {
+				let attribute = xml.attributes.item(j);
+				obj["attr"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType === 3) { // text
+		obj = xml.nodeValue;
+	}
+
+	// do children
+	if (xml.hasChildNodes()) {
+		for(let i = 0; i < xml.childNodes.length; i++) {
+			let item = xml.childNodes.item(i);
+			let nodeName = item.nodeName;
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					let old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
+};
+
+export default xmlToJson
